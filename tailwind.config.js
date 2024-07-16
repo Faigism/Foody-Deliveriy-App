ffb53c069c56a3e8220a7a793aded0bcd102a340
Faig Ismayilov <faigyi@code.edu.az>
2024-07-17 01:16:39 +0400

    tailwindcss added

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './share/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
