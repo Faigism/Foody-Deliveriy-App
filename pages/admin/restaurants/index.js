204fb300e96a2f56838b42be5ce06fabf6f2ee59
Samandar <itissmndr@gmail.com>
2024-07-20 13:16:43 +0400

    showAllRestaurants added

--- pages/admin/restaurants/index.js
+++ pages/admin/restaurants/index.js
@@ -1,14 +1,25 @@
 "use client"
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import RestaurantItems from '../../../shared/components/admin/RestaurantItems'
 import Subheading from '../../../shared/components/admin/Subheading'
 import AdminLayout from '../../../shared/components/layout/admin'
-import { getCategoriesFromDB, getRestaurants, getRestaurantsFromDB } from '../../../shared/services/axios';
+import { getCategoriesFromDB, getRestaurants } from '../../../shared/services/axios';
 
 const Restaurants = () => {
   const [categories, setCategories] = useState([]);
   const [restaurants, setRestaurants] = useState([]);
+
+  const showAllRestaurants = async () => {
+    const allRestaurants = await getRestaurants();
+    console.log(allRestaurants.data.result.data)
+    const restaurants = allRestaurants.data.result.data;
+    setRestaurants(restaurants)
+
+  }
+  useEffect(() => {
+    showAllRestaurants();
+  }, [])
 
   const getRestaurantsByCategory = async (categoryId) => {
     const restaurantsData = await getRestaurants();
