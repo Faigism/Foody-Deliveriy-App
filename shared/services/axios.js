466becc3560bf422f627e7f4cb2607fd7d0f6b1d
Faig Ismayilov <faigyi@code.edu.az>
2024-07-24 16:39:15 +0400

    adminCategory page fetch edit and svg added

--- shared/services/axios.js
+++ shared/services/axios.js
@@ -57,6 +57,7 @@ export const deleteRestaurantById = async (id) => {
   }
 }
 
+//Categories
 export const getCategoriesFromDB = async () => {
   try {
     const response = await instanceAxios.get('/category')
@@ -65,6 +66,15 @@ export const getCategoriesFromDB = async () => {
     //   self.indexOf(value) === index);
     // console.log(uniqueCategories)
     return categories
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const postCategory = async (form) => {
+  try {
+    const response = await instanceAxios.post('/category', form)
+    return response
   } catch (err) {
     console.log(err)
   }
@@ -105,6 +115,7 @@ export const deleteCategories = async (id) => {
     console.log(err)
   }
 }
+//Categories
 
 export const getOffer = async () => {
   try {
